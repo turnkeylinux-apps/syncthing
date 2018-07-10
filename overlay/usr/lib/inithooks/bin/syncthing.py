ee2910e221bf9a21e78064b6d0fa1b64bec7bf61
Jeremy Davis <jeremy@turnkeylinux.org>
2018-07-10 10:45:56 +1000

    refactor inithooks, move inital processing to firstboot.d/20regen-syncthing-secrets

--- overlay/usr/lib/inithooks/bin/syncthing.py
+++ overlay/usr/lib/inithooks/bin/syncthing.py
@@ -36,27 +36,15 @@ def main():
         d = Dialog('TurnKey Linux - First boot configuration')
         password = d.get_password(
             "Syncthing Password",
-            "Enter Web GUI Password.")
+            "Enter new password for the 'syncthing' Web UI user.")
 
     hashpw = bcrypt.hashpw(password, bcrypt.gensalt())
-    system('systemctl', 'enable', 'syncthing@syncthing.service')
-    system('systemctl', 'start', 'syncthing@syncthing.service') 
-    system('sleep 10')
 
-    """Remove All Users and Password If Any"""
-    system('sed', '-i', "/<user>.*/d", '/home/syncthing/.config/syncthing/config.xml')
-    system('sed', '-i', "/<password>.*/d", '/home/syncthing/.config/syncthing/config.xml')
-
-    """Edit to Listen On All Interfaces"""
-    system('sed', '-i', "s/<address>[0-9].*<\/address>/<address>0.0.0.0:8384<\/address>/g", '/home/syncthing/.config/syncthing/config.xml')
-    """Add Default User syncthing"""
-    system('sed', '-i', "/<address>[0-9].*<\/address>/ a \\\t<user>syncthing</user>", '/home/syncthing/.config/syncthing/config.xml')
     """Assign password to user syncthing based off input"""
-    system('sed', '-i', "/<user>syncthing<\\/user>/ a \\\t<password>%s</password>" % hashpw, '/home/syncthing/.config/syncthing/config.xml')
-    """Enable SSL"""
-    system('sed', '-i', "s/tls=\"false\"/tls=\"true\"/g", '/home/syncthing/.config/syncthing/config.xml')
+    system('sed', '-i', '\|<user>syncthing</user>|!b;n;c \\t<password>%s</password>' % hashpw, '/home/syncthing/.config/syncthing/config.xml')
     """Restart Syncthing"""
     system('systemctl', 'restart', 'syncthing@syncthing.service')
+
 if __name__ == "__main__":
     main()
 
